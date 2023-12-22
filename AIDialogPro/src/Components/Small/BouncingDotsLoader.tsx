1880f4bf13f1f3044ff774b5f3fa25bcb5419d7e
Piatekf <filip.piatek2005@gmail.com>
2023-12-22 21:37:06 +0100

    Images deleted and some installations

---
+++ AIDialogPro/src/Components/Small/BouncingDotsLoader.tsx
@@ -0,0 +1,11 @@
+const BouncingDotsLoader = () => {
+    return (
+        <div className="bouncing-dots-loader">
+            <div className="bouncing-dots-loader-dot"></div>
+            <div className="bouncing-dots-loader-dot"></div>
+            <div className="bouncing-dots-loader-dot"></div>
+        </div>
+    )
+}
+
+export default BouncingDotsLoader
