591211e06fa663521dfffc2faf38129c57652135
Piatekf <filip.piatek2005@gmail.com>
2024-02-15 10:56:50 +0100

    tailwind styling and

--- AIDialogPro/src/App.tsx
+++ AIDialogPro/src/App.tsx
@@ -1,47 +1,56 @@
 import './App.css'
-import {useState, useRef, useEffect} from 'react'
-import Header from "./Components/Layout/Header.tsx";
-import Body from "./Components/Layout/Body.tsx";
-import Footer from "./Components/Layout/Footer.tsx";
-import axios from 'axios'
-import getHistory from "./Services/getHistory.ts";
+import { useState, useRef, useEffect } from 'react'
+import Header from './Components/Layout/Header.tsx'
+import Body from './Components/Layout/Body.tsx'
+import Footer from './Components/Layout/Footer.tsx'
+import getHistory from './Services/getHistory.ts'
+import TMessage from './Types/TMessage.ts'
+import { MdOutlineChatBubble } from 'react-icons/md'
 
 function App() {
-    const [show, setShow] = useState(false)
-    const [messages, setMessages] = useState<any[]>([])
-    const [loading, setLoading] = useState(false)
-    const lastMessageRef = useRef<HTMLDivElement>(null)
-    const handleClose = () => {
-        setShow(false)
-    }
-    const handleSetMessages = (new_messages: any[]) => {
-        console.log("Nowe wiadomości:")
-        console.log(new_messages)
-        if(new_messages.length === 1 && new_messages[0]['role'] === 'user'){
-            console.log("Ustawiam wiadomość użytkownika")
-            setMessages(prevState => [...prevState, new_messages[0]])
-        }
-        else{
-            console.log("Ustawiam wszystkie wiadomości")
-            setMessages(new_messages)
-        }
-    }
-    const fetchMessages = async() => {
-        try{
-            const chatHistory = await getHistory()
-            if (chatHistory){
-                setMessages(chatHistory)
-            }
-        } catch(err){
-            console.log(err)
-        }
-    }
+  const [show, setShow] = useState(false)
+  const [messages, setMessages] = useState<TMessage[]>([])
+  const [loading, setLoading] = useState(false)
+  const lastMessageRef = useRef<HTMLDivElement>(null)
+  const handleClose = () => {
+    setShow(false)
+  }
 
-    useEffect(() => {
-        fetchMessages()
-    },[])
-    return (
+  const handleSetMessages = (new_messages: any[]) => {
+    console.log('Nowe wiadomości:')
+    console.log(new_messages)
+    if (new_messages.length === 1 && new_messages[0]['role'] === 'user') {
+      console.log('Ustawiam wiadomość użytkownika')
+      setLoading(true)
+      setMessages((prevState) => [...prevState, new_messages[0]])
+    } else {
+      setLoading(false)
+      console.log('Ustawiam wszystkie wiadomości')
+      setMessages(new_messages)
+    }
+  }
 
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [loading, messages, lastMessageRef])
+  const fetchMessages = async () => {
+    try {
+      console.log('Ładowanie: ' + loading)
+      const chatHistory = await getHistory()
+      if (chatHistory) {
+        setMessages(chatHistory)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchMessages()
+  }, [])
+  return (
     <>
         <div className={show ? `chatbot-box active` : `chatbot-box`}>
             <Header handleClose={handleClose} />
@@ -52,6 +61,22 @@ function App() {
             <span>ask</span>
         </div>
 
+      <div
+        className={`flex flex-col bg-neutral-50 w-80 h-110 absolute right-5 bottom-5 rounded-lg shadow-lg overflow-hidden transition-all duration-300 ${show ? 'opacity-100' : 'opacity-0 -translate-y-10'}`}
+      >
+        <Header handleClose={handleClose} />
+        <Body
+          messages={messages}
+          loading={loading}
+          lastMessageRef={lastMessageRef}
+        />
+        <Footer loading={loading} setMessages={handleSetMessages} />
+      </div>
+      <div
+        className={`fixed right-5 bottom-5 rounded-full p-4 bg-red-700 cursor-pointer shadow-md ${show && 'hidden'}`}
+        onClick={() => setShow(!show)}
+      >
+      </div>
     </>
   )
 }
