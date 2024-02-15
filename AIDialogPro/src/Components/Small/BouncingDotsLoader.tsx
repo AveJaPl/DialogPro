c9e20268ae8c8b24f978620c346751ff5f48d785
Piatekf <filip.piatek2005@gmail.com>
2024-02-15 13:06:13 +0100

    Loader component created

--- AIDialogPro/src/Components/Small/BouncingDotsLoader.tsx
+++ AIDialogPro/src/Components/Small/BouncingDotsLoader.tsx
@@ -1,13 +1,13 @@
 const BouncingDotsLoader = () => {
-    return (
-        <div className="flex p-2 items-center">
-            <div className="flex space-x-2">
-                <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce"></div>
-                <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce200"></div>
-                <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce400"></div>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="flex p-2 items-center">
+      <div className="flex space-x-2">
+        <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce"></div>
+        <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce200"></div>
+        <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce400"></div>
+      </div>
+    </div>
+  )
+}
 
-export default BouncingDotsLoader;
+export default BouncingDotsLoader
