024acf00445f0995314f4e00a004276d8f6e2c1b
Piatekf <filip.piatek2005@gmail.com>
2024-02-15 10:57:17 +0100

    tailwind styling

--- AIDialogPro/src/Components/Layout/Body.tsx
+++ AIDialogPro/src/Components/Layout/Body.tsx
@@ -1,45 +1,60 @@
-import BouncingDotsLoader from "../Small/BouncingDotsLoader.tsx";
+import BouncingDotsLoader from '../Small/BouncingDotsLoader.tsx'
+import {useEffect} from "react";
 
 type BodyProps = {
-    messages: any[],
-    loading: boolean,
-    lastMessageRef: any
+  messages: any[]
+  loading: boolean
+  lastMessageRef: any
 }
 
 type Message = {
-    role: string,
-    content: string,
-    time: string
+  role: string
+  content: string
+  time: string
 }
 
 const Body = (props: BodyProps) => {
-    return (
-        <div className={`chatbot-box-body`}>
-            <div className={`chatbot-box-body-message`}>
-                {props.messages && props.messages.map((message: Message,index: number, arr: string[])=>(
-                        <div
-                            key={index}
-                            ref={index === arr.length - 1 ? props.lastMessageRef : null}
-                        >
-                            <div className={message['role'] === 'user' ? 'message user' : 'message'}>
-                                <div className={`chatbot-box-body-message-text`}>
-                                    {message['content']}
-                                </div>
-                                <div className={`chatbot-box-body-message-time`}>
-                                    {message['time']}
-                                </div>
-                            </div>
+  //   foreach message set random time in format HH:MM
+    for (let i = 0; i < props.messages.length; i++) {
+        let h = (Math.floor(Math.random() * 24)).toString()
+        let m = (Math.floor(Math.random() * 60)).toString()
+        if(h.length === 1) h = '0' + h
+        if(m.length === 1) m = '0' + m
+        props.messages[i].time = `${h}:${m}`
+
+    }
+  return (
+      <div className="flex flex-grow overflow-auto p-4 custom-scrollbar">
+        <div className="space-y-2 w-full">
+          {props.messages &&
+              props.messages.map(
+                  (message: Message, index: number, arr: any[]) => (
+                      <div
+                          key={index}
+                          ref={index === arr.length - 1 ? props.lastMessageRef : null}
+                          className={`flex break-words ${message.role === 'user' ? 'justify-end' : 'justify-start'} mb-2`}
+                      >
+                        <div className={`min-w-28 max-w-60 mb-2 px-4 py-2 rounded-lg ${
+                            message.role === 'user' ? 'bg-red-500 text-white rounded-br-none' : 'bg-gray-200 rounded-bl-none '
+                        }`}>
+                          <div className="text-sm">
+                            {message.content}
+                          </div>
+                          <div className={`text-2xs ${message.role === 'user' ? 'text-white': 'text-black'} text-right mt-1`}>
+                            {message.time}
+                          </div>
                         </div>
-                    )
-                )}
-                {props.loading && (
-                    <div ref={props.lastMessageRef}>
-                        <BouncingDotsLoader />
-                    </div>
-                )}
-            </div>
+                      </div>
+                  ),
+              )}
+          {props.loading && (
+              <div ref={props.lastMessageRef} className={`w-full`}>
+                <BouncingDotsLoader />
+              </div>
+          )}
         </div>
-    );
+      </div>
+  )
 }
 
-export default Body;
+export default Body
