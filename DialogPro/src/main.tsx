faaed37a3e057d82177f54243c4af9fe6bd7f4b0
Piatekf <filip.piatek2005@gmail.com>
2024-02-16 09:57:09 +0100

    change to root

--- DialogPro/src/main.tsx
+++ DialogPro/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { ContextProvider } from './Context/messagesContext.tsx'
 
-ReactDOM.createRoot(document.getElementById('chatbot-box')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ContextProvider>
       <App />
