72f49d101788a67b714436d5211bdaf13368f1bc
Piatekf <filip.piatek2005@gmail.com>
2024-02-16 09:56:54 +0100

    footer simplified

--- DialogPro/src/Components/Layout/Footer.tsx
+++ DialogPro/src/Components/Layout/Footer.tsx
@@ -1,43 +1,33 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { IoIosSend } from 'react-icons/io'
 import Copyright from '../Small/Copyright.tsx'
 import getAnswer from '../../Services/getAnswer.ts'
 import { useMessages } from '../../Context/messagesContext.tsx'
+import TMessage from '../../Types/TMessage.ts'
 
 const Footer = () => {
-  const [input, setInput] = useState<string>('')
-  const [sent, setSent] = useState<boolean>(false)
-
+  const [input, setInput] = useState('')
   const { setMessages, loading, setLoading } = useMessages()
-  const handleInput = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value)
-  }, [])
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleSendMessage(input)
-    }
   }
 
-  const handleSendMessage = async (input: string) => {
+  const handleSendMessage = async () => {
     if (input.trim() === '') {
       return
     }
     setLoading(true)
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      {
-        id: 'generatedID',
-        role: 'user',
-        content: input,
-        timestamp: new Date().toLocaleTimeString(),
-      },
-    ])
-    setSent(true)
-  }
-
-  useEffect(() => {
-    const getAnswerFromServer = async (user_prompt: string) => {
-      let { chat, message, status } = await getAnswer(user_prompt)
+    const newMessage: TMessage = {
+      id: 'generatedID', // Rozważ implementację generowania unikatowych ID
+      role: 'user',
+      content: input,
+      timestamp: new Date().toLocaleTimeString(),
+    }
+    setMessages((prevMessages) => [...prevMessages, newMessage])
+    setInput('') // Czyść input tutaj, aby użytkownik widział, że wiadomość została "wysłana"
+    try {
+      const { chat, message, status } = await getAnswer(input)
       if (status === 200) {
         setMessages(chat)
       } else {
@@ -51,15 +41,17 @@ const Footer = () => {
           },
         ])
       }
-      setLoading(false)
+    } catch (error) {
+      console.error('Error sending message:', error)
     }
-    if (sent) {
-      getAnswerFromServer(input).then(() => {
-        setInput('')
-      })
-      setSent(false)
+    setLoading(false)
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !loading) {
+      handleSendMessage()
     }
-  }, [sent])
+  }
 
   return (
     <div className={`w-full flex flex-col border-t-2 p-2`}>
@@ -67,14 +59,14 @@ const Footer = () => {
         <input
           className={`text-sm w-5/6 border-2 p-2 rounded-lg active:outline-none focus:outline-none focus:border-red-500`}
           placeholder={loading ? 'Pisze...' : 'Wpisz wiadomość...'}
-          onChange={(e) => handleInput(e)}
+          onChange={handleInput}
           value={input}
-          onKeyDown={(e) => handleKeyDown(e)}
+          onKeyDown={handleKeyDown}
           disabled={loading}
         />
         <button
           className={`flex items-center justify-center w-1/6 bg-red-500 text-white rounded-lg ml-2`}
-          onClick={() => handleSendMessage(input)}
+          onClick={handleSendMessage}
           disabled={loading}
         >
           <IoIosSend />
